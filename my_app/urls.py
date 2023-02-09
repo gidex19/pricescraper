e62ad0de386ff7ca21fce08dce2c894978baa549
gidex19 <gideonozu2015@gmail.com>
2023-02-09 13:16:33 +0100

    konga scraper function completed

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,4 +3,5 @@ from . import views as my_app_view
 
 urlpatterns = [
     path('home/', my_app_view.home, name='homepage'),
+    # path('testpage/', my_app_view.jsontest, name='jsonpage'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,6 +3,7 @@ from .forms import SearchForm
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
 # Create your views here.
 
@@ -10,25 +11,60 @@ def getHTML(link):
     response = requests.get(link)
     print("scraping HTML......")
     # print(response.text.pre)
-    doc = BeautifulSoup(response.text, "html.parser" )
-    print(doc.prettify())
-    return doc
+    soup = BeautifulSoup(response.text, "html.parser" )
+    # print(doc.prettify())
+    # print(response.text)
+    print("print out...")
+    content = soup.find('script', {"id": "__NEXT_DATA__"}).string
+    dictionary = json.loads(content) 
+    check = dictionary["props"]["initialProps"]["pageProps"]["resultsState"]["content"]["hits"]
+    for item in check:
+        print("===============================================================")
+        print(item['name'])
+        print(item['price'])
+        print(item['product_type'])
+        print(item['url_key'])
+        print(item['price'])
+        print(item['description'])
+        print(item['rating'])
+        print("===============================================================")
+        print("     ")
+    # print(check)
+    return soup.find('script', {"id": "__NEXT_DATA__"})
+    # return soup.prettify("utf-8")
+
+
+
 
 # getHTML("https://www.jumia.com.ng/catalog/?q=tecno+camon+19")
 # jumia_search = "https://www.jumia.com.ng/catalog/?q=tecno+camon+19"
-
+# https://www.konga.com/search?search=redmi%20note%2010
 
 def home(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data.get('search_field')
-            data = data.replace(' ', '+')
-            jumia_search = "https://www.jumia.com.ng/catalog/?q="
-            rdata = jumia_search + data
+            jumia_search_string = data.replace(' ', '+')
+            konga_search_string = data.replace(' ', '%20')
+            jumia_url = "https://www.jumia.com.ng/catalog/?q="
+            konga_url = "https://www.konga.com/search?search="
+            jumia_purl = jumia_url + jumia_search_string
+            konga_purl = konga_url + konga_search_string
             # print(rdata)
             # print(data)
-            getHTML(rdata)
+            # j = getHTML(jumia_purl)
+            k = getHTML(konga_purl)
+            print("form submitted")
+            # print(k)
+            # with open('konga.html', 'wb') as f:
+            #     f.write(k)
+            #     f.close()
+            #     print("konga file saved")
+            # with open('jumia.html', 'wb') as f:
+            #     f.write(j)
+            #     f.close()    
+            #     print("jumia file saved")
     else:
         form = SearchForm()        
     return render(request, 'my_app/home.html', {'form': form})
